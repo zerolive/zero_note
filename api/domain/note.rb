5292921fd8e4afca7731736d70fd2f84b7b7c249
zerolive <zerolive@gmail.com>
2018-01-27 23:21:12 +0100

    Note has a default type

--- api/domain/note.rb
+++ api/domain/note.rb
@@ -1,11 +1,25 @@
 
 class Note
-  def initialize(text:'', type:'')
+  DEFAULT_TYPE = 'task'
+
+  def initialize(text:'', type:DEFAULT_TYPE)
     @text = text
     @type = type
   end
 
   def serialize
-    { 'text' => @text, 'type' => @type }
+    { 'text' => @text, 'type' => type }
+  end
+
+  private
+
+  def type
+    return DEFAULT_TYPE if no_type?
+
+    @type
+  end
+
+  def no_type?
+    @type.nil? || @type.empty?
   end
 end
--- api/spec/end2end/note_spec.rb
+++ api/spec/end2end/note_spec.rb
@@ -21,4 +21,15 @@ describe 'Note' do
     expect(new_note['text']).to eq(note['text'])
     expect(new_note['type']).to eq(note['type'])
   end
+
+  it 'has a default type' do
+    default_type = 'task'
+    note = { 'text' => 'any_text' }
+
+    post '/new_note', note
+
+    new_note = JSON.parse(last_response.body)
+    expect(new_note['text']).to eq(note['text'])
+    expect(new_note['type']).to eq(default_type)
+  end
 end
