ae65f1ecf8cc7cbec13bce591b8531adfe5d4e96
zerolive <zerolive@gmail.com>
2018-01-28 16:57:17 +0100

    Create helper methods for notes

--- api/spec/end2end/notes_spec.rb
+++ api/spec/end2end/notes_spec.rb
@@ -14,10 +14,10 @@ describe 'Notes' do
 
   it 'are requested for today' do
     allow(Infrastructure::Moment).to receive(:today).and_return(today)
-    create_note_for_today
-    create_done_note_for_today
-    create_note_for_yesterday
-    create_note_done_yesterday
+    create_undone_note(today)
+    create_undone_note(yesterday)
+    create_done_note(today)
+    create_done_note(yesterday)
 
     post '/notes_for_today'
 
@@ -25,12 +25,12 @@ describe 'Notes' do
     expect(notes.size).to eq(3)
   end
 
-  def create_note_for_today
+  def create_undone_note(day)
     note = {
       'id' => 'an_id',
       'text' => 'text',
       'type' => 'task',
-      'created_at' => today,
+      'created_at' => day,
       'due_date' => 'any_date',
       'done_date' => ''
     }
@@ -38,40 +38,14 @@ describe 'Notes' do
     Infrastructure::Client.insert_one(note)
   end
 
-  def create_done_note_for_today
+  def create_done_note(day)
     note = {
       'id' => 'an_id',
       'text' => 'text',
       'type' => 'task',
-      'created_at' => today,
+      'created_at' => day,
       'due_date' => 'any_date',
-      'done_date' => today
-    }
-
-    Infrastructure::Client.insert_one(note)
-  end
-
-  def create_note_for_yesterday
-    note = {
-      'id' => 'an_id',
-      'text' => 'text',
-      'type' => 'task',
-      'created_at' => yesterday,
-      'due_date' => 'any_date',
-      'done_date' => ''
-    }
-
-    Infrastructure::Client.insert_one(note)
-  end
-
-  def create_note_done_yesterday
-    note = {
-      'id' => 'an_id',
-      'text' => 'text',
-      'type' => 'task',
-      'created_at' => yesterday,
-      'due_date' => 'any_date',
-      'done_date' => yesterday
+      'done_date' => day
     }
 
     Infrastructure::Client.insert_one(note)
